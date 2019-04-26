140f79c437c6f6c08bae26c112cc87de54648356
CaminusPrime <Cbd_98@hotmail.com>
2019-04-27 01:04:38 +1000

    Added "Hello world", some comments for later structure

--- main.c
+++ main.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+
+
+
 int main()
 {
+	printf("Hello world");
+
+
+
 
 return 0;
 }
+
+
+// Encryption Function I
+
+// Encryption Function II
+
+// Decryption Function I
+
+// Decryption Function II
