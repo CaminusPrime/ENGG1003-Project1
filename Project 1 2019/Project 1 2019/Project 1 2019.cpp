6ef02314b41e96bf5f0709b4b6d5615f0b781aaf
CaminusPrime <Cbd_98@hotmail.com>
2019-04-28 23:36:49 +1000

    More progress in structure and UI flow. slowing down to work on actual functionality of program.

--- Project 1 2019/Project 1 2019/Project 1 2019.cpp
+++ Project 1 2019/Project 1 2019/Project 1 2019.cpp
@@ -1,15 +1,30 @@
-// Project 1 2019.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// C3258834 - Cameron Down
+// Programming assignment 1
 
 #include "pch.h"
 #include <math.h>
 #include <string>
 #include <iostream>
+#include <stdio.h>
+
+
+double ENCroCipher();
+double DECroCipher();
 
 
 int main()
 {
-	int x;
+	/*
+	Variable description:
+	int- "x" used for choice selection, "cr" used for rotation cipher.
+
+
+
+	*/
+
+	// variable declaring
+	int x, cr; 
+	//char str[30];
 	
 	printf("ENGG1003 Project 1\nC3258834 - Cameron Down\n");
 	printf("--------------------------------------------\n");
@@ -24,47 +39,81 @@ int main()
 		printf("Please enter 1, 2 or 3\n");
 		scanf_s("%d", &x);
 	}
-	printf("%d", x);
-
-
+		else if (x = 1)
 	//Section I (Hard code process)
 	{
-	printf("\n>>> READING HARD CODE SAMPLE\n");
+
+		printf("\n--------------------------------------------\n");
+		printf("\n>>> READING HARD CODE SAMPLE\n");
 	
-	
+		return (0);
 	} 
 
-
+	if (x = 2)
 	//Section II (User input process)
 	{
-		printf("\nPlease type message below:\n");
+		
+		printf("\n--------------------------------------------\n");
+		printf("\n>>> READING FROM USER INPUT\nPlease type message below:\n");
+		//scanf_s("%s", str);
 
-		printf("Would you like to use?\n 1 - Rotation cipher\n 2 - Substitution cipher");
-		// scan for 1 or 2
-		// if 1
+		printf("Would you like to use?\n 1 - Rotation cipher\n 2 - Substitution cipher\n");
+	
+		scanf_s("%d", &x);
+		if (x >= 3)
+		{
+			printf("Please enter 1 or 2\n");
+			scanf_s("%d", &x);
+		}
+		if (x = 1)
 		{
 			//Rotation Cipher output
-			printf("Enter rotation amount:");
+			printf("Enter rotation amount:\n");
 			// scan amount as x
+			scanf_s("%d", &cr);
+
+			if (cr >= 26)
+			{
+				printf("Please enter a number up to 25\n");
+				scanf_s("%d", &cr);
+			}
+			//printf("%d", cr);
 			// function call
-			printf(">>> ENCRYPTED");
-			// print output 
+
+
+
+
+
+
+			printf(">>> ENCRYPTED\n");
+			// print enncrypted output 
+			return (0);
 		}
 		
 		// if 2
 		{
 
 		}
+
+		printf("\n--------------------------------------------\n");
 	}
 
 
-	/*//Section III (file reading processes)
+	/*
+	if (x = 3)
+	//Section III (file reading processes)
 	{
+	
+	printf("\n--------------------------------------------\n");
 	printf("\n>>> READING FILE");
 	int a;
 	scanf("%d", &a);
 	printf(">>> FILE FOUND\n%d", a);
 	
+	printf("\n--------------------------------------------\n");
+	
+	
+		return (0);
 	}
 	*/
 
@@ -97,16 +146,33 @@ int main()
 
 
 // Rotation cipher encryption 
+double ENCroCipher;
+{
+	char result;
 
+
+
+	return result;
+}
 
 // Rotation cipher decryption 
+double DECroCipher;
+{
 
+
+}
 
 
 // Substitution cipher encryption
+double ENCsubCipher;
+{
 
+}
 // Substitution cipher decryption
+double DECsubCipher;
+{
 
+}
 
 //decrpyption with rotation cipher using text only
 
